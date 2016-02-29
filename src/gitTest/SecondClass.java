7d0e5a79c8f60400c2fed4cf9a674ff4317b8a3c
maiara.reinaldo <maiara.reinaldo@cds.eb.mil.br>
2016-02-29 15:57:16 -0300

    Added exclusive feature to new branch

--- src/gitTest/SecondClass.java
+++ src/gitTest/SecondClass.java
@@ -3,5 +3,6 @@ package gitTest;
 public class SecondClass {
 	public static void main(String[] args) {
 		System.out.println("Teste git");
+		System.out.println("Funcionalidade unica do new");
 	}
 }
